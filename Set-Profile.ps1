40463721ca6434e3d8cafcfc161d5a563f0f0e48
sirsycho <51727075+sirsycho@users.noreply.github.com>
2019-06-11 18:43:09 -0600

    Update Set-Profile.ps1

--- Set-Profile.ps1
+++ Set-Profile.ps1
@@ -1,4 +1,4 @@
-$profilePath = 'https://raw.githubusercontent.com/mjisaak/azure/master/profile.ps1'
+$profilePath = 'https://github.com/sirsycho/PowerShell/blob/master/profile.ps1'
  
 $downloadString = '{0}?{1}' -f $profilePath, (New-Guid)
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($profilePath))
